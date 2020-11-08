e4bf97be2cdfaddeca2da66e21cf580344a2ebc2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-08 23:42:59 +0100

    feat: style issuepage download button

--- classes/DownloadButtonRenderer.php
+++ classes/DownloadButtonRenderer.php
@@ -2,6 +2,9 @@
 
 class DownloadButtonRenderer {
   function renderForPost($post) {
-    return '<div class="downloadbtn"><a href="/">(Pobierz tekst)</a></div>';
+    $downloadUrl = get_post_meta($post->ID, 'issuepage_download_url');
+    if(empty($downloadUrl) || !isset($downloadUrl[0])) return '';
+
+    return '<p class="issuepage-downloadbtn"><a href="' . $downloadUrl[0] . '">Pobierz PDF</a></p>';
   }
 }
