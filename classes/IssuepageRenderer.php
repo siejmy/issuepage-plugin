3d796b83560730b00bcaeda99c66e852520374e8
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-08 22:03:58 +0100

    feat: issuepage content base styles

--- classes/IssuepageRenderer.php
+++ classes/IssuepageRenderer.php
@@ -1,7 +1,38 @@
 <?php
 
 class IssuepageRenderer {
+  function __construct() {
+    require_once( ABSPATH . 'wp-content/plugins/siejmycommon-plugin/classes/ImageRenderer.php');
+  }
+
   function renderPost($post) {
-    return '<main class="issuepage" role="main">Issuepage</main>';
+    return $this->renderImg($post)
+         . $this->renderContent($post);
+  }
+
+  function renderImg($post) {
+    $mediaId     = get_post_thumbnail_id($post);
+    $imgRenderer = new ImageRenderer();
+    $alt = $post->post_title;
+    return
+        '<div class="issuepage-img" id="' . $this->getHeroId($post) . '">'
+      .   $imgRenderer->renderImgByAttachmentId($mediaId, $alt)
+      . '</div>'
+      . $this->getHeroPreloadingBgStyles($imgRenderer, $post, $mediaId);
+  }
+
+  function renderContent($post) {
+    return '<div class="issuepage-content">'
+              . '<h1>' . $post->post_title . '</h1>'
+              . $post->post_content
+         . '</div>';
+  }
+
+  function getHeroPreloadingBgStyles($imgRenderer, $post, $mediaId) {
+    return '<style>#' . $this->getHeroId($post) . ' { background-image: url(' . $imgRenderer->getFallbackDataSrc($mediaId) . '); }</style>';
+  }
+
+  function getHeroId($post) {
+    return 'posthero_' . $post->ID;
   }
 }
