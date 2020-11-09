c18e06f4eca67208dafeecc6d53de289301a76ac
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-09 15:44:56 +0100

    feat: show download counter in metabox

---
+++ classes/Config.php
@@ -0,0 +1,5 @@
+<?php
+
+class Config {
+ static $statsUrl = "https://siejmy.pl/cfw/counter_stats.4wFw4W0TZ5tq9Fg5.json";
+}
--- metaboxes/issuepage_metabox_html.php
+++ metaboxes/issuepage_metabox_html.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once(dirname(__FILE__) . '/../classes/Config.php');
+
 function issuepage_metabox_html( $post ) {
   $issuepage_download_url = get_post_meta( $post->ID, 'issuepage_download_url', true );
   $issuepage_issue_no = get_post_meta( $post->ID, 'issuepage_issue_no', true );
 
-    ?>
+    ?><p>
     <label for="issuepage_download_url">Url pobierania numeru</label>
     <input
       name="issuepage_download_url"
@@ -19,5 +21,32 @@ function issuepage_metabox_html( $post ) {
       type="text"
       value="<?php echo($issuepage_issue_no) ?>"
     />
+</p>
     <?php
+    echo render_stats_box();
+}
+
+function render_stats_box() {
+  return
+    ' <p><i>Pamiętaj, aby poprzedzić url pliku za pomocą \'/get\' aby były liczone pobrania. Dodatkowo usuń \'https://siejmy.pl\' z początku pliku. Url powinien zaczynać się od \'/\'. Prawidłowy format URLa: \'/get/wp-content/uploads/2020/10/SIEJMY-6.2020.pdf\'</i></p>'
+    . '<strong>Siejmy licznik pobrań (aktualizowane co 5 minut): </strong>'
+    . ' <iframe src="' . Config::$statsUrl . '" title="Siejmy download stats" width="450" height="150" style="border: 1px solid blue;"></iframe>';
+    /*
+  <div id="statsbox">
+			<pre id="statsoutput"></pre>
+			<script>
+				window.onload = () => {
+					fetch("https://siejmy.pl/cfw/counter_stats.4wFw4W0TZ5tq9Fg5.json")
+						.then((text) => resp.text())
+						.then(
+							(text) =>
+								(document.getElementById("statsoutput").innerText = text),
+						)
+						.catch(
+							(err) =>
+								(document.getElementById("statsoutput").innerText = err.stack),
+						)
+				}
+			</script>
+    </div>';*/
 }
